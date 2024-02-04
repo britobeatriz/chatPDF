48a6b9581878f54fbc35818c3e6554775e2a44eb
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-02-03 22:12:06 -0300

    word to pdf

--- chatpdf.py
+++ chatpdf.py
@@ -1,4 +1,5 @@
 import os
+import aspose.words as aw
 
 files = os.listdir('./')
 #print(files)
@@ -6,3 +7,6 @@ files = os.listdir('./')
 curriculum = [file for file in files if file.endswith('.docx')][0]
 #print(curriculum)
 
+#transform word to pdf
+doc = aw.Document(curriculum)
+doc.save("curriculum.pdf")
