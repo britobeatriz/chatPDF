fba4f0acd531f5aab4d83448623e1051f23923c7
Beatriz de Brito Lima <beatriz.lima@cvm.gov.br>
2024-02-08 15:34:51 -0300

    placeholder sidebar

--- app.py
+++ app.py
@@ -7,22 +7,34 @@ st.dataframe(data)
 # Add a slider to the sidebar:
 box_local = st.sidebar.selectbox(
     'Buscar candidato por localidade',
-    (data['localidade'])
+    (data['localidade']),
+    index=None,
+    placeholder="Selecione a localidade",
 )
 box_profession = st.sidebar.selectbox(
     'Buscar candidato por profissão',
-    (data['profissao'])
+    (data['profissao']),
+    index=None,
+    placeholder="Selecione a profissão"
 )
 box_office = st.sidebar.selectbox(
     'Buscar candidato por cargo',
     (data['cargo_atual']),
+    index=None,
+    placeholder="Selecione o cargo"
 )
-
 #visualização-filtro
 filter_local = data[data['localidade'] == box_local]
-st.write(filter_local)
-
-if box_local != '':
+if box_local == True:
+    st.write("Filtro:")
     st.write(filter_local)
-else:
-    st.write(data)
+
+# filter_profession = data[data['profissao'] == box_profession]
+# if box_profession:
+#     st.write("Filtro:")
+#     st.write(filter_profession)
+
+# filter_office = data[data['cargo_atual'] == box_office]
+# if box_office:
+#     st.write("Filtro:")
+#     st.write(filter_office)
